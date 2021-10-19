d2a3e1e94b70c7e643da3a209224d30987ad4f77
hoangan2268 <92203264+hoangan2268@users.noreply.github.com>
2021-10-20 02:46:56 +0700

    Add files via upload

---
+++ hw4.js
@@ -0,0 +1,15 @@
+BAI 1:
+// let firstName = 'Nguyen ';
+// let surName = 'Hoang '
+// let lastName = "An";
+// alert('Your full name is: '+ firstName + surName + lastName)
+// console.log(firstName + surName + lastName)
+
+BAI 2:
+// alert("64 divided by 7 has left: " + String(64%7) )
+// console.log(String(64%7) )
+
+BAI 3:
+// let text = "Hello World"
+// alert("Word length is: " + String(text.length))
+// console.log("Word length is: " + String(text.length))
